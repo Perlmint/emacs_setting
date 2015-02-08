9384306a99c6fec44ceaccabd19014ce07169e95
Gyusun Yeom <omniavinco@gmail.com>
2015-02-08 17:39:15 +0800

    Initial commit

---
+++ init.el
@@ -0,0 +1,53 @@
+ ;; disable startup message
+(setq inhibit-startup-message t)
+
+ ;; show line number
+(global-linum-mode t)
+
+ ;; package repo
+(require 'package)
+;(add-to-list 'package-archives
+;    '("marmalade" .
+;      "http://marmalade-repo.org/packages/"))
+(package-initialize)
+
+
+ ;; setup load-path
+(add-to-list 'load-path "~/.emacs.d/lisp")
+(let ((default-directory "~/.emacs.d/lisp/"))
+  (normal-top-level-add-subdirs-to-load-path))
+
+ ;; require all .el files in ~/.emacs.d/lisp/mode
+(mapc (lambda (name)
+        (require (intern (file-name-sans-extension name))))
+      (directory-files "~/.emacs.d/lisp/mode" nil "\\.el$"))
+
+(add-to-list 'auto-mode-alist '("\\.lua$" . lua-mode))
+(add-to-list 'interpreter-mode-alist '("lua" . lua-mode))
+
+ ;; tab width
+(setq default-tab-width 4)
+
+ ;; theme
+(load-theme 'solarized-dark t)
+
+ ;; blank-mode
+(require 'blank-mode)
+(setq blank-chars (list 'tabs 'spaces 'trailing 'lines-tail 'empty))
+(global-blank-mode 1)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Setup short cuts
+;
+
+ ;; disable embeded ime change
+(global-set-key (kbd "S-SPC") nil)
+
+ ;; kill rectangle
+(global-set-key (kbd "C-x r k") 'kill-rectangle)
+
+ ;; insert into rectangle
+(global-set-key (kbd "C-x r i") 'string-insert-rectangle)
+
+(require 'json-reformat)
+(require 'android-mode)
