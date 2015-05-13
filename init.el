39529399a1afc5a205a612bc388760c7c5dd5afb
Gyusun Yeom <omniavinco@gmail.com>
2015-05-13 11:18:03 +0900

    Add embeded tidy and beautify-xml-region

--- init.el
+++ init.el
@@ -49,6 +49,22 @@
 (setq blank-chars (list 'tabs 'spaces 'trailing 'lines-tail 'empty))
 (global-blank-mode 1)
 
+ ;; tidy path
+(if (eq system-type 'windows-nt)
+  (setq tidy-shell-command
+    (expand-file-name "~/.emacs.d/others/bin/win/tidy.exe"))
+)
+
+ ;; beautify-xml
+(defun beautify-xml-region (begin end)
+  (interactive "r")
+  (save-excursion
+      (nxml-mode)
+      (goto-char begin)
+      (while (search-forward-regexp "\>[[:blank:]]*\<" nil t) 
+        (backward-char) (insert "\n"))
+      (indent-region begin end)))
+
 (server-start)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
