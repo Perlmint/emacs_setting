c589953a281b337f62722de22307cfeab23b8c1b
Gyusun Yeom <omniavinco@gmail.com>
2016-02-25 17:12:49 +0900

    Fix python warnings on windows

--- config/modes/python.el
+++ config/modes/python.el
@@ -5,3 +5,5 @@
   (add-to-list 'company-backends 'company-jedi))
 
 (add-hook 'python-mode-hook 'setup-python-mode)
+(setq python-shell-prompt-detect-failure-warning nil)
+(setq python-shell-completion-native-enable nil)
