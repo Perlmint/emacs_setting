482d7e647bd1ee9ccb0eaf334ec2204798dbe2db
Gyusun Yeom <omniavinco@gmail.com>
2017-01-19 01:17:45 +0900

    save auto generated

--- init.el
+++ init.el
@@ -35,3 +35,19 @@
 (require 'magit-exttool)
 (require 'markdown-preview-mode)
 (require 'ng2-mode)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(frame-background-mode (quote dark))
+ '(magit-merge-arguments (quote ("--no-ff")))
+ '(package-selected-packages
+   (quote
+    (magit yasnippet web-mode undo-tree twittering-mode tidy tide slack rust-mode python-mode omnisharp markdown-mode+ magit-popup irony git-commit editorconfig csv-mode company-racer company-quickhelp company-jedi company-go color-theme-solarized cmake-mode))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
