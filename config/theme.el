0cb29aa562dbb9a6c647c90c74ada0506f5232c9
Gyusun Yeom <gyusun@nextfloor.com>
2017-02-23 12:41:16 +0900

    set font with candidates list

--- config/theme.el
+++ config/theme.el
@@ -6,8 +6,21 @@
       '(frame-background-mode (quote dark)))
     (load-theme 'solarized t)
 
+    (defun font-existsp (font)
+      (if (null (x-list-fonts font))
+        nil font))
+    (defun font-candidate (&rest fonts)
+      (when fonts
+        (font-existsp (car fonts))
+        (apply 'font-candidate (cdr fonts))
+        )
+      )
+
     ;; set default-font
     ;; https://github.com/googlei18n/noto-ckj
-    (set-default-font "Noto Sans Mono CJK KR-10")
+    (set-default-font
+      (font-candidate
+        "Noto Sans Mono CJK KR-10"
+        "Noto Sans Mono CJK KR Regular-10"))
     )
   )
